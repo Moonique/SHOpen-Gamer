7efc2c4e96b66dc2535c76c063fa33bbcd23f939
Daria Shchetinina <mooneuq@gmail.com>
2020-05-07 20:35:05 +0700

    Добавлена разметка экрана логина

--- app/src/main/java/com/example/shopengamerapp/SplashActivity.kt
+++ app/src/main/java/com/example/shopengamerapp/SplashActivity.kt
@@ -7,7 +7,7 @@ import android.os.Handler
 
 class SplashActivity : Activity() {
 
-    private val SPLASHTIMEOUT: Long = 15000
+    private val SPLASHTIMEOUT: Long = 3000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
