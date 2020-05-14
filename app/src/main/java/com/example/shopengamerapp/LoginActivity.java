7e5fa4e3d76ea87b42deba2c3958681fbeb0516f
Daria Shchetinina <mooneuq@gmail.com>
2020-05-14 16:07:18 +0700

    Добавлено боковое меню и фрагменты

--- app/src/main/java/com/example/shopengamerapp/LoginActivity.java
+++ app/src/main/java/com/example/shopengamerapp/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.shopengamerapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.Nullable;
@@ -30,7 +31,12 @@ public class LoginActivity extends Activity {
         inputTeamColor = findViewById(R.id.team_color_input);
         buttonLogin = findViewById(R.id.login_button);
 
-        buttonLogin.setOnClickListener(v -> login());
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
     }
 
     private void login() {
--- app/src/main/java/com/example/shopengamerapp/MainActivity.java
+++ app/src/main/java/com/example/shopengamerapp/MainActivity.java
@@ -1,14 +1,72 @@
 package com.example.shopengamerapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SearchFragment()).commit();
+
+        navigationView.setCheckedItem(R.id.nav_item_one);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_item_one:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new SearchFragment()).commit();
+                        break;
+                    case R.id.nav_item_two:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new GalleryFragment()).commit();
+                        break;
+                    case R.id.nav_item_three:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new CallFragment()).commit();
+                        break;
+                    case R.id.close:
+                        finish();
+                        break;
+                }
+
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START))
+            drawer.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
     }
 }
